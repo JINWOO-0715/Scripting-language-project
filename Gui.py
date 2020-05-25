a8da78393a6a314b8a9bcb812bce7132e67d1eb8
unknown <pjw1480@naver.com>
2020-05-25 17:26:12 +0900

    UI 배치
    UI샘플 이미지 배치완료

--- Gui.py
+++ Gui.py
@@ -1,47 +1,94 @@
 from tkinter import *
 from tkinter import font
 import tkinter.messagebox
-
-window = Tk()
-window.geometry("800x700")
+g_Tk = Tk()
+g_Tk.geometry("800x800")
+DataList = []
 
 def InitTopText():
-    TempFont = font.Font(window, size=20, weight='bold', family = 'Consolas')
-    MainText = Label(window, font = TempFont, text="[서울시 영화 검색 App]")
+    TempFont = font.Font(g_Tk, size=20, weight='bold', family = 'Consolas')
+    MainText = Label(g_Tk, font = TempFont, text="[서울시 영화관 검색 App]")
     MainText.pack()
     MainText.place(x=250)
-
-
 def InitSearchListBox():
     global SearchListBox
-    ListBoxScrollbar = Scrollbar(window)
+    ListBoxScrollbar = Scrollbar(g_Tk)
     ListBoxScrollbar.pack()
     ListBoxScrollbar.place(x=150, y=50)
 
-    TempFont = font.Font(window, size=15, weight='bold', family='Consolas')
-    SearchListBox = Listbox(window, font=TempFont, activestyle='none',
+    TempFont = font.Font(g_Tk, size=15, weight='bold', family='Consolas')
+    SearchListBox = Listbox(g_Tk, font=TempFont, activestyle='none',
                             width=10, height=1, borderwidth=12, relief='ridge',
                             yscrollcommand=ListBoxScrollbar.set)
 
-    SearchListBox.insert(1, "영화관")
-    SearchListBox.insert(2, "영화차트")
-    SearchListBox.insert(3, "영화검색")
+    SearchListBox.insert(1, "영화차트")
+    SearchListBox.insert(2, "영화검색")
+    SearchListBox.insert(3, "영화관")
     SearchListBox.pack()
     SearchListBox.place(x=10, y=50)
     ListBoxScrollbar.config(command=SearchListBox.yview)
-
 def InitInputLabel():
     global InputLabel
-    TempFont = font.Font(window, size=15, weight='bold', family = 'Consolas')
-    InputLabel = Entry(window, font = TempFont, width = 26, borderwidth = 12, relief = 'ridge')
+    TempFont = font.Font(g_Tk, size=15, weight='bold', family = 'Consolas')
+    InputLabel = Entry(g_Tk, font = TempFont, width = 22, borderwidth = 12, relief = 'ridge')
     InputLabel.pack()
-    InputLabel.place(x=180, y=48)
+    InputLabel.place(x=180, y=50)
 
+def InitSearchButton():
+    TempFont = font.Font(g_Tk, size=12, weight='bold', family = 'Consolas')
+    SearchButton = Button(g_Tk, font = TempFont, text="검색",  command=SearchButtonAction)
+    SearchButton.pack()
+    SearchButton.place(x=480, y=52)
+
+
+def SearchButtonAction():
+    pass
+def SearchLibrary():
+    pass
+def InitRenderText():
+    global RenderText
+
+    RenderTextScrollbar = Scrollbar(g_Tk)
+    RenderTextScrollbar.pack()
+    RenderTextScrollbar.place(x=375, y=200)
+
+    TempFont = font.Font(g_Tk, size=10, family='Consolas')
+    RenderText = Text(g_Tk, width=40, height=20, borderwidth=12, relief='ridge', yscrollcommand=RenderTextScrollbar.set)
+    RenderText.pack()
+    RenderText.place(x=10, y=140)
+    RenderTextScrollbar.config(command=RenderText.yview)
+    RenderTextScrollbar.pack(side=RIGHT, fill=BOTH)
+
+    RenderText.configure(state='disabled')
+    RenderText = Text(g_Tk, width=40, height=20, borderwidth=12, relief='ridge', yscrollcommand=RenderTextScrollbar.set)
+    RenderText.pack()
+    RenderText.place(x=400, y=140)
+    RenderTextScrollbar.config(command=RenderText.yview)
+    RenderTextScrollbar.pack(side=RIGHT, fill=BOTH)
+
+    RenderText.configure(state='disabled')
+    RenderText = Text(g_Tk, width=40, height=20, borderwidth=12, relief='ridge', yscrollcommand=RenderTextScrollbar.set)
+    RenderText.pack()
+    RenderText.place(x=10, y=450)
+    RenderTextScrollbar.config(command=RenderText.yview)
+    RenderTextScrollbar.pack(side=RIGHT, fill=BOTH)
+
+    RenderText.configure(state='disabled')
+    RenderText = Text(g_Tk, width=40, height=20, borderwidth=12, relief='ridge', yscrollcommand=RenderTextScrollbar.set)
+    RenderText.pack()
+    RenderText.place(x=400, y=450)
+    RenderTextScrollbar.config(command=RenderText.yview)
+    RenderTextScrollbar.pack(side=RIGHT, fill=BOTH)
+
+    RenderText.configure(state='disabled')
 
 
 InitTopText()
 InitSearchListBox()
 InitInputLabel()
-window.mainloop()
-
-
+InitSearchButton()
+InitRenderText()
+#InitSendEmailButton()
+#InitSortListBox()
+#InitSortButton()
+g_Tk.mainloop()
