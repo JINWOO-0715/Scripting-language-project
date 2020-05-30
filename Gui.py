42ec2b31c6de56b13c56aa5dcff1c3a3463a25d3
unknown <pjw1480@naver.com>
2020-05-30 14:15:59 +0900

    상세정보창
    UI배치 완료
    기능 데모 구현완료

--- Gui.py
+++ Gui.py
@@ -69,60 +69,54 @@ class GUI:
         self.movie_ranking_list_box =None
         self.movie_ranking_list_box_scrollbar = Scrollbar(self.window)
         self.movie_ranking_list_box_scrollbar.pack()
-        self.movie_ranking_list_box_scrollbar.place(x=450, y=50)
+        self.movie_ranking_list_box_scrollbar.place(x=200, y=140)
 
         TempFont = font.Font(self.window, size=15, weight='bold', family='Consolas')
         self.movie_ranking_list_box = Listbox(self.window, font=TempFont, activestyle='none',
                                 width=25, height=10, borderwidth=12, relief='ridge')
 
         self.movie_ranking_list_box.pack()
-        self.movie_ranking_list_box.place(x=400, y=140)
+        self.movie_ranking_list_box.place(x=10, y=140)
         self.movie_ranking_list_box_scrollbar.config(command=self.movie_ranking_list_box.yview)
         for i in self.movie_ranking:
             self.movie_ranking_list_box.insert(10,"["+i['rank']+"위]" + i['movieNm'])
 
     def find(self):
         num =self.movie_ranking_list_box.curselection()
-        print(self.movie_ranking_list_box.get(num[0]))
-        
+        return num[0]
 
     def InitMovieRankingText(self):
-        self.movie_ranking_text_scrollbar = Scrollbar(self.window)
-        self.movie_ranking_text_scrollbar.pack()
-        self.movie_ranking_text_scrollbar.place(x=200, y=20)
+        self.movie_information_scrollbar = Scrollbar(self.window)
+        self.movie_information_scrollbar.pack()
+        self.movie_information_scrollbar.place(x=500, y=450)
         TempFont = font.Font(self.window, size=10, family='Consolas')
-
         # 영화 텍스트
-        self.movie_ranking_text = Text(self.window, width=40, height=20, borderwidth=12, relief='ridge',
-                                  yscrollcommand= self.movie_ranking_text_scrollbar.set)
-        self.movie_ranking_text.pack()
-        self.movie_ranking_text.place(x=10, y=140)
-        self.movie_ranking_text_scrollbar.config(command=self.movie_ranking_text.yview)
-        self.movie_ranking_text_scrollbar.pack(side=RIGHT, fill=BOTH)
-        self. movie_ranking_text.configure(state='disabled')
+        self.movie_imformation_text = Text(self.window, width=40, height=20, borderwidth=12, relief='ridge',
+                                           yscrollcommand= self.movie_information_scrollbar.set)
+        self.movie_imformation_text.pack()
+        self.movie_imformation_text.place(x=10, y=450)
+        self.movie_information_scrollbar.config(command=self.movie_imformation_text.yview)
+        self.movie_information_scrollbar.pack(side=RIGHT, fill=BOTH)
+        self. movie_imformation_text.configure(state='disabled')
 
-
-
-        #self.movie_imformation_text = Text(self.window, width=40, height=20, borderwidth=12, relief='ridge')
         #self. movie_favorites_list_text = Text(self.window, width=40, height=20, borderwidth=12, relief='ridge')
 
 
     def SearchButtonAction(self):# 랭킹 출력함수
-        self.movie_ranking_text.configure(state='normal')
-        for i in self.movie_ranking:
-            self.movie_ranking_text.insert(INSERT,"[")
-            self.movie_ranking_text.insert(INSERT,i['rank']+"위")
-            self.movie_ranking_text.insert(INSERT, "] ")
-            self.movie_ranking_text.insert(INSERT, i['movieNm'] )
-            self.movie_ranking_text.insert(INSERT, " ")
-            self.movie_ranking_text.insert(INSERT, "\n")
-            self.movie_ranking_text.insert(INSERT, "개봉일: ")
-            self.movie_ranking_text.insert(INSERT, i['openDt'])
-            self.movie_ranking_text.insert(INSERT, " ")
-            self.movie_ranking_text.insert(INSERT, "누적 관람객: ")
-            self.movie_ranking_text.insert(INSERT, i['audiAcc'])
-            self.movie_ranking_text.insert(INSERT, "\n\n")
-            self.find()
+        self.movie_imformation_text.configure(state='normal')
+        i =int( self.find())
+        self.movie_imformation_text.insert(INSERT, self.movie_ranking[i]['movieNm'])
+        self.movie_imformation_text.insert(INSERT, ": ")
+        self.movie_imformation_text.insert(INSERT, "\n")
+        self.movie_imformation_text.insert(INSERT, "개봉일: ")
+        self.movie_imformation_text.insert(INSERT, self.movie_ranking[i]['openDt'])
+        self.movie_imformation_text.insert(INSERT, " ")
+        self.movie_imformation_text.insert(INSERT, "\n")
+        self.movie_imformation_text.insert(INSERT, "누적 관람객: ")
+        self.movie_imformation_text.insert(INSERT, self.movie_ranking[i]['audiAcc'])
+        self.movie_imformation_text.insert(INSERT, "\n")
+        self.movie_imformation_text.insert(INSERT, "d: ")
+        self.movie_imformation_text.insert(INSERT, self.movie_ranking[i]['audiAcc'])
 
     def SearchLibrary(self):
         pass
