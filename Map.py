1015fdc46716f24a29adb010a2bdbaa3ae0a0568
unknown <pjw1480@naver.com>
2020-06-17 00:32:57 +0900

    영화 상세 검색
    영화 상세 검색 구현 및 리스트박스 배치

--- Map.py
+++ Map.py
@@ -4,13 +4,10 @@ import webbrowser
 
 
 def Pressed(x,y):
-    # 위도 경도 지정
-    x = float(s['좌표정보(X)'].iloc[index])
-    y =float(s['좌표정보(Y)'].iloc[index])
-
-    map_osm = folium.Map(location=[x,y])
+    map_osm = folium.Map(location=[x,y] ,  zoom_start=40)
     # 마커 지정
     folium.Marker([x,y], popup='찾는위치').add_to(map_osm)
     # html 파일로 저장
     map_osm.save('osm.html')
     webbrowser.open_new('osm.html')
+Pressed()
