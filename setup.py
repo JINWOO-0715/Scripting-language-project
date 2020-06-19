7d5a935c8402e2f97c3c701d14901e8e0b89f328
gyuhyun-jang <55976879+gyuhyun-jang@users.noreply.github.com>
2020-06-20 02:24:06 +0900

    배포 구현

---
+++ setup.py
@@ -0,0 +1,7 @@
+from distutils.core import setup
+
+setup(name='Seoul Movie',
+      version='1.0',
+      py_modules=['GetPoster','Gmail','Gui','main','make_graph','Map','Naver_Crowling',
+                  'ReadFile','Search','telegram','UI']
+      )
